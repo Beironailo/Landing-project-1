ae77cc9fed848a8ea90fbe2cbb1b43c1088b6460
Tufra <60471036+Tufra@users.noreply.github.com>
2021-06-09 20:35:28 +0300

    chat v0.1

---
+++ Temp project/block1+2 + chat/js/chat.js
@@ -0,0 +1,63 @@
+let ws;
+let url;
+
+function initWebSocket() {
+    try {
+        ws = new WebSocket(url);
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+function addMessage(message) {
+    let tag = "<div class=\"message-right\">" + message + "</div>";
+    console.log(tag);
+
+    let container = document.querySelector("#message_container");
+    container.innerHTML += tag;
+    container.scrollTop = container.scrollHeight;
+
+}
+
+function addMessageList(messages) {
+    for (let message in messages) {
+        addMessage(message);
+    }
+}
+
+document.querySelector("#submit").
+        addEventListener("click", function () {
+
+            let message = document.querySelector("#message_input").value;
+            addMessage(message);
+        });
+
+document.querySelector(".close_button").
+        addEventListener("click", function () {
+            let chat = document.querySelector(".chat");
+            let button = document.querySelector(".chat_icon");
+
+            chat.style.display = "none";
+            button.style.display = "";
+        });
+
+document.querySelector(".chat_icon").
+        addEventListener("click", function () {
+            let chat = document.querySelector(".chat");
+            let button = document.querySelector(".chat_icon");
+
+            chat.style.display = "";
+            button.style.display = "flex";
+        });
+
+document.querySelector(".chat_icon").
+        addEventListener("mouseover", function () {
+            let button = document.querySelector(".chat_icon");
+            button.style.boxShadow = "0 0 20px 0 #0db79b";
+        });
+
+document.querySelector(".chat_icon").
+        addEventListener("mouseout", function () {
+           let button = document.querySelector(".chat_icon");
+           button.style.boxShadow = "";
+        });
