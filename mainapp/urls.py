a51dda6effc752557975daf3df6aa7e98692f08b
Lumbril <32036622+Lumbril@users.noreply.github.com>
2021-06-16 22:10:05 +0300

    files for heroku

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat/<str:room_name>/', views.RegisterView.as_view(), name='userchat'),
+    path('', views.RegisterView.as_view(), name='homepage'),
+    path('chat/<str:room_name>/', views.index, name='userchat'),
     path('chat/', views.admin_chat, name='adminchat')
 ]
